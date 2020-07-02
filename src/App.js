307d2406424714ea48dcfa0298f90f0a13102ba9
rony646 <rony646@users.noreply.github.com>
2020-07-02 13:13:08 -0300

    axios interceptors fix

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Layout from './components/Layout/Layout'
 import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder'
 
 class App extends Component {
+
   render() {
     return (
       <div>
